e5ee7f6e6518615b397f253a1aaf4f47839daf76
Breno Farias <41799984+brenofarias@users.noreply.github.com>
2024-03-29 19:18:56 -0300

    Criando as classes Produto e Categoria

---
+++ APICatalogo/Controllers/Models/Categoria.cs
@@ -0,0 +1,8 @@
+﻿namespace APICatalogo.Controllers.Models;
+
+public class Categoria
+{
+    public int CategoriaID { get; set; }
+    public string? Nome { get; set; }
+    public string? ImagemURL { get; set; }
+}
