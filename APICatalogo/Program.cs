78d77ac4749b0d9dfbdc9319492a1b5ba75f7635
Breno Farias <41799984+brenofarias@users.noreply.github.com>
2024-03-29 21:07:02 -0300

    Registrando os serviços no container DI

--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -1,3 +1,6 @@
+using APICatalogo.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,12 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Add conection with DataBase
+string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(mySqlConnection,
+    ServerVersion.AutoDetect(mySqlConnection)));
+    
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
