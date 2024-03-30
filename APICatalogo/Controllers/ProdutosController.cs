5c31efb4fc77179335bd2a37561f7bb71371b4a2
Breno Farias <41799984+brenofarias@users.noreply.github.com>
2024-03-30 20:26:51 -0300

    ProdutosController - POST

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -29,7 +29,7 @@ namespace APICatalogo.Controllers
             return produtos;
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name ="ObterProduto")]
         public ActionResult<Produto> Get(int id)
         {
             var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoID == id);
@@ -40,6 +40,30 @@ namespace APICatalogo.Controllers
             }
 
             return produto;
+        }
+
+        [HttpPost]
+        public ActionResult Post(Produto produto)
+        {
+            // Verifica se existe dados
+            if(produto is null)
+                return BadRequest();
+
+            // Recupera todos os produtos
+            var produtos = _context.Produtos.ToList();
+            // Verifica se já existe um produto com o mesmo nome
+            bool existe = produtos.Any(p => p.Nome == produto.Nome);
+            if (existe)
+                return BadRequest("Produto já existe!");
+
+            // Cria na memória
+            _context.Produtos.Add(produto);
+            // Salva no banco
+            _context.SaveChanges();
+
+            // Retorna os dados do produto cadastrado na rota Get(id)
+            return new CreatedAtRouteResult("ObterProduto",
+                new { id = produto.ProdutoID }, produto);
         }
     }
 }
