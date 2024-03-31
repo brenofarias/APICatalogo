71288ba764a25839cbeaf7c68e3e693da8d95b2d
Breno Farias <41799984+brenofarias@users.noreply.github.com>
2024-03-30 22:00:41 -0300

    Ajustes e Otimizações - Serialização JSON

--- APICatalogo/Controllers/Models/Produto.cs
+++ APICatalogo/Controllers/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace APICatalogo.Controllers.Models;
 
@@ -29,6 +30,7 @@ public class Produto
     
     // Definindo chave estrangeira
     public int CategoriaId { get; set; }
+    [JsonIgnore]
     public Categoria? Categoria { get; set; }   
 
 }
